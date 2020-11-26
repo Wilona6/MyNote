81ce2f303a234636d23c3aa6bb9f5bf62aac668c
Wilona6 <Wilona6@outlook.com>
2020-11-27 01:11:32 +0800

    mynote

---
+++ README.md
@@ -0,0 +1,29 @@
+# 简易记事本
+
+## 说明
+
++ 实现
+  + 语言： Android
+  + 数据库：SQL Lite
++ 功能
+  1. 添加记事本
+  2. 修改记事本
+  3. 删除记事本
+## 效果
+
+- 广告页
+
+  ![image](https://github.com/xmenw/Note/blob/master/image/S90607-16112113.png)
+
+- 首页
+
+  ​	![image](https://github.com/xmenw/Note/blob/master/image/S90607-16112719.png)
+
+- 添加页面
+
+  ​	![image](https://github.com/xmenw/Note/blob/master/image/S90607-16114922.png)
+
+- 修改页面
+
+  	![image](https://github.com/xmenw/Note/blob/master/image/S90607-16120075.png)
+
---
+++ app/src/main/java/com/example/administrator/note/SettingActivity.java
@@ -0,0 +1,41 @@
+package com.example.administrator.note;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText editText;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        initViews();
+    }
+
+    private void initViews() {
+        editText = (EditText) findViewById(R.id.author);
+        button = (Button) findViewById(R.id.save_author);
+        button.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.save_author:
+                String author = editText.getText().toString().trim();
+                SharedPreferences.Editor editor = getSharedPreferences("author", MODE_PRIVATE).edit();
+                editor.putString("author", author);
+                editor.apply();
+                Toast.makeText(getApplicationContext(), "修改成功!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+    }
+}
